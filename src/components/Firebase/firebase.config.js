cd6f7c926518419ddbf4d62d53ee35320b16b6d5
jaber4770 <jaber.ahmed4770@gmail.com>
2022-01-30 20:29:31 +0600

    firebase config and initialization done

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB9viMicO40mcLPhhatimBJE5IdX8wZxB0",
+    authDomain: "burj-al-arab-bc343.firebaseapp.com",
+    projectId: "burj-al-arab-bc343",
+    storageBucket: "burj-al-arab-bc343.appspot.com",
+    messagingSenderId: "104776892571",
+    appId: "1:104776892571:web:ca4e738722d8ddf15a063e"
+};
+
+export default firebaseConfig;
